8dec389044685b6283f4ded0a94fbfdbdecceaaa
sainiteshGit <nitesh_4u@yahoo.com>
2023-04-12 20:38:51 -0400

    update the openai charbot

--- embeddings/obtainDataSet.py
+++ embeddings/obtainDataSet.py
@@ -5,7 +5,7 @@ import openai
 from openai.embeddings_utils import get_embedding
 openai.api_key = 'YOUR_OPEN_API_KEY'
 
-##### embedding model parameters
+####### embedding model parameters
 embedding_model = "text-embedding-ada-002"
 embedding_encoding = "cl100k_base"  # this the encoding for text-embedding-ada-002
 max_tokens = 8000  # the maximum for text-embedding-ada-002 is 8191
@@ -30,7 +30,10 @@ encoding = tiktoken.get_encoding(embedding_encoding)
 
 # omit reviews that are too long to embed
 df["n_tokens"] = df.combined.apply(lambda x: len(encoding.encode(x)))
+
+
 df = df[df.n_tokens <= max_tokens].tail(top_n)
+
 len(df)
 
 df["embedding"] = df.combined.apply(lambda x: get_embedding(x, engine=embedding_model))
