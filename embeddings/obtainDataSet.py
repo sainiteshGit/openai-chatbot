45c9d74e480258a21f6db2639420c64c3158f623
sainiteshGit <nitesh_4u@yahoo.com>
2023-05-04 20:37:10 -0400

    openai chatbot update

--- embeddings/obtainDataSet.py
+++ embeddings/obtainDataSet.py
@@ -5,7 +5,7 @@ import openai
 from openai.embeddings_utils import get_embedding
 openai.api_key = 'YOUR_OPEN_API_KEY'
 
-################################################################ embedding model parameters #############################################################
+#################################################################### embedding model parameters ##################################################################
 embedding_model = "text-embedding-ada-002"
 embedding_encoding = "cl100k_base"  # this the encoding for text-embedding-ada-002
 max_tokens = 8000  # the maximum for text-embedding-ada-002 is 8191
